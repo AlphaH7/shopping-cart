1953bf444838de24616a9df4fb9fa6c7b524a395
AlphaH7 <Alistier.hea@gmail.com>
2017-04-28 17:30:53 +0530

    autoselect for filter

--- app/scripts/controllers/home.ctrl.js
+++ app/scripts/controllers/home.ctrl.js
@@ -5,8 +5,9 @@ angular.module('AM').controller('homeCtrl',['$scope','$http','$mdToast',function
       s.items = r.data;
     }
   );
-  s.sort = function(keyname){
-    s.sortKey = keyname;   //set the sortKey to the param passed
+  s.sort = function(){
+    console.log(s.filter);
+    s.sortKey = s.filter;   //set the sortKey to the param passed
     s.reverse = !s.reverse; //if true make it false and vice versa
   }
 }]);
